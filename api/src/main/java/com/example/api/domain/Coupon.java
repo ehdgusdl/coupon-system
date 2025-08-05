e2535a4cce3891a5498fec5336368b4a5e42d174
DHKim7762 <149556729+ehdgusdl@users.noreply.github.com>
2025-08-06 03:40:10 +0900

    쿠폰 엔티티 생성

--- api/src/main/java/com/example/api/domain/Coupon.java
+++ api/src/main/java/com/example/api/domain/Coupon.java
@@ -3,7 +3,7 @@ package com.example.api.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id
+import jakarta.persistence.Id;
 
 @Entity
 public class Coupon {
